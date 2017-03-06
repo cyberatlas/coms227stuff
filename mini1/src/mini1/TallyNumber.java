0d100b6ae322133771b55d18b1560e407f1b5443
Alex <ruski550@gmail.com>
2017-03-06 17:42:25 -0600

    more struggling

--- mini1/src/mini1/TallyNumber.java
+++ mini1/src/mini1/TallyNumber.java
@@ -1,54 +1,56 @@
 package mini1;
 
+import com.sun.deploy.util.StringUtils;
+
 public class TallyNumber {
 
+    //integer representation of the tally stuff
     private int intRep;
+    //string representation of the tally stuff
     private String strRep;
 
     private int num5;
     private int num1;
 
-    private String str5 ="*";
+    private String str5 = "*";
     private String str1 = "|";
 
-    public TallyNumber(int givenValue)
-    {
+    public TallyNumber(int givenValue) {
         intRep = givenValue;
-
+        num5 = givenValue / 5;
+        num1 = givenValue % 5;
+        strRep = repeat(str5, num5) + repeat(str1, num1);
     }
 
-    public TallyNumber(String givenString)
-    {
+
+    public TallyNumber(String givenString) {
         strRep = givenString;
+        int len = strRep.length(); //Find the length of the string so we can iterate through a loop and count the characters in it
+
+
+        for (int i =0; i < len; i++){
+
+        }
     }
 
-    public String getStringValue()
-    {
+    public String getStringValue() {
         return strRep;
     }
 
-    public  int getIntValue()
-    {
-
+    public int getIntValue() {
         return intRep;
     }
 
-    public static void add(TallyNumber other)
-    {
+    public static void add(TallyNumber other) {
 
     }
 
-    public static void combine(TallyNumber other)
-    {
+    public static void combine(TallyNumber other) {
 
     }
 
-    public void normalize()
-    {
+    public void normalize() {
 
     }
-
-    num5 = givenValue/5;
-    num1 = givenValue%5;
-    strRep = ("*" * num5)+ ("|" * num1);
 }
+
