5cb8186c5fb86416e2fa96497b13d80d1a1d553b
Alex <ruski550@gmail.com>
2017-03-06 09:06:20 -0600

    Save 1

--- mini1/src/mini1/TallyNumber.java
+++ mini1/src/mini1/TallyNumber.java
@@ -2,4 +2,23 @@ package mini1;
 
 public class TallyNumber {
 
+	private int intRep;
+	private String strRep;
+
+	public TallyNumber(int givenValue){	
+		intRep = givenValue;
+	}
+	public TallyNumber(String givenString){
+		strRep = givenString;
+	}
+	
+	public static String getStringValue(){}
+
+	public  static int getIntValue(){}
+
+	public static void add(TallyNumber other){}
+
+	public static void combine(TallyNumber other){}
+
+	public void normalize(){}
 }
