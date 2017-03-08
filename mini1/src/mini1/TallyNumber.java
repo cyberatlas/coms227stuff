3696a34c405f398a77afae433ab79323cd98efbf
Alex <ruski550@gmail.com>
2017-03-08 00:06:27 -0600

    Ended up with 27/40 :(

--- mini1/src/mini1/TallyNumber.java
+++ mini1/src/mini1/TallyNumber.java
@@ -10,46 +10,55 @@ public class TallyNumber {
 
     private int num5;
     private int num1;
-
+    
+    private boolean b1 = false;
+    private boolean b2 = false;
+    
     private int numSpace;
 
     public TallyNumber(int givenValue) {
         intRep = givenValue;
-        num5 = givenValue / 5;
-        num1 = givenValue % 5;
-        //strRep = str5.repeat(num5) + str1.repeat(num1);
-        strRep = (intRep !=0) ? repeat("*", num5) + repeat("|", num1) : "0";
+        strRep = convertToString(intRep);//sets the string representation equal to the return of the convertT
+        b2 = true;
     }
 
 
     public TallyNumber(String givenString) {
 
-        strRep = (givenString);
+        strRep = givenString;
         convertToInt(strRep);
-        
-
+        b1 = true;
     }
-    
-    private int convertToInt(String strRep){
+    private String convertToString(int givenValue){
+        //defining holdString as a temporary String and then returns that String 
+    	
+    	String holdString = "";
+    	num5 = givenValue / 5;
+        num1 = givenValue % 5;
+        //strRep = str5.repeat(num5) + str1.repeat(num1);
+        holdString = (intRep !=0) ? repeat("*", num5) + repeat("|", num1) : "0";
+        return holdString;
+    }
+    private int convertToInt(String str){
     	int sum =0;
-        countSpaces();
-        for (int i = 0; i < strRep.length(); i++) {
-            if (strRep.charAt(i) == '|') {
+        numSpace = countSpaces(strRep);
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) == '|') {
                 //intRep += 1;
                 sum +=1;
-            } else if (strRep.charAt(i) == '*') {
+            } else if (str.charAt(i) == '*') {
                 //intRep += 5;
                 sum+=5;
-            } else if (strRep.charAt(i) == ' ') {
+            } else if (str.charAt(i) == ' ') {
                 intRep += sum * (int) Math.pow(10, numSpace);
                 numSpace -=1;
                 sum = 0;
             }
-            else if (strRep.charAt(i) =='0'){
+            else if (str.charAt(i) =='0'){
                 sum += 0;
             }
 
-            else if ((strRep.charAt(i) != '|') &&  (strRep.charAt(i) != '*' )&& (strRep.charAt(i) != ' ')){
+            else if ((str.charAt(i) != '|') &&  (str.charAt(i) != '*' )&& (str.charAt(i) != ' ')){
                 intRep =-1;
                 strRep = "";
                 sum = 0;
@@ -58,10 +67,16 @@ public class TallyNumber {
         }
         intRep = (strRep == "") ? -1: intRep + sum ;
         return intRep;
+        //return intRep;
     }
 
     public String getStringValue() {
-        return strRep;
+        if(b1 == false){
+    	normalize();
+    	return strRep;
+        }
+        else return strRep;
+    	
     }
 
     public int getIntValue() {
@@ -69,28 +84,65 @@ public class TallyNumber {
     }
 
     public void add(TallyNumber other) {
-
+    	intRep += other.getIntValue();
     }
 
     public void combine(TallyNumber other) {
-
+    	/*b1 = true;
+        String[] s1 = getStringValue().split(" ");
+        String[] s2 = other.getStringValue().split(" ");
+        String combine = "";
+        int i = 0;
+        if(s1.length > s2.length) {
+            for (i = 0; i < s1.length - s2.length; i++) combine += s1[i] + " ";
+            for(int j=0;j<s2.length;j++) combine += s1[i+j] + s2[j] + " ";
+        }
+        else {
+            for (i = 0; i < s2.length - s1.length; i++) combine += s2[i] + " ";
+            for(int j=0;j<s1.length;j++) combine +=  s1[j] + s2[i+j] + " ";
+        }
+        strRep = combine.substring(0, combine.length()-1);*/
+    	getStringValue();
+    	strRep = strRep + other.getStringValue();
     }
 
     public void normalize() {
-    	String r;
+    	//just a dummy string that we can add values to
     	
+    	if (b1 == false){
+    	
+    	int intRep2 = getIntValue(); //another int to hold the value of intRep
+		String int2String = Integer.toString(intRep2);//stores the integer as a string
+		int atVal = 0;//stores the current value of the int in the String
+		String normie = "";//Stores the normalized string
+		
+    	if (getIntValue() > 0){
+    	    		
+    		for(int i =0; i <int2String.length(); i++){
+    		atVal = (int) int2String.charAt(i) - '0';
+    		if (atVal == 0) {normie += "0";}
+    		normie += repeat("*", (atVal/5))+ repeat("|", (atVal%5))+ " ";
+    	}
+    			  		
+    	}
+    	if (getIntValue() == 0){
+    		normie = "0" + " ";
+    	}
+    	strRep = normie.trim();
+   }
     }
     //found this method, allows me to make a string that has a character repeated, some number of times
     private static String repeat(String str, int times){
         return new String(new char[times]).replace("\0", str);
     }
-    private int countSpaces(){
-        for (int i =0; i <strRep.length(); i++) {
-            if (strRep.charAt(i) == ' '){
-                numSpace +=1;
+    private int countSpaces(String str){
+       int space= 0;
+    	for (int i =0; i <str.length(); i++) {
+            if (str.charAt(i) == ' '){
+                space +=1;
             }
         }
-        return numSpace;
+        return space;
     }
 }
 
--- mini1/src/mini1/TallyNumberTest.java
+++ mini1/src/mini1/TallyNumberTest.java
@@ -6,11 +6,15 @@ package mini1;
 public class TallyNumberTest {
     public static void main(String[] args){
 
-        TallyNumber t =  new TallyNumber("*************");
-        System.out.println(t.getIntValue());
+        TallyNumber t =  new TallyNumber(64);
+      //  System.out.println(t.getIntValue());
         System.out.println(t.getStringValue());
-        TallyNumber t2 = new TallyNumber("** ||");
+        /*TallyNumber t2 = new TallyNumber("** ||");
         System.out.println(t2.getStringValue());
         System.out.println(t2.getIntValue());
+        TallyNumber t3 = new TallyNumber("7");
+        System.out.println(t3.getIntValue());
+        System.out.println(t3.getStringValue());*/
+
     }
 }
