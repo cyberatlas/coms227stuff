7ad79fed1a3668887f7debb80915fdbe24056a54
Alex <ruski550@gmail.com>
2017-03-22 21:17:06 -0500

    Got the first part of checkpoint1 good to go

--- project8/src/lab8/LineNumberer.java
+++ project8/src/lab8/LineNumberer.java
@@ -1,11 +1,17 @@
 package lab8;
+
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileNotFoundException;
 
 public class LineNumberer
 {
-  public static void main(String[] args)
+  public static void main(String[] args) throws FileNotFoundException
   {
-    Scanner scanner = new Scanner(System.in);
+	 File  file = new File("C:\\Users\\ruski\\Documents\\Coms227\\coms227shit\\project7\\src\\lab7\\Deck.java");
+	 Scanner scanner = new Scanner(file);
+    //Scanner scanner = new Scanner(System.in);
     int lineCount = 1;
 
     while (scanner.hasNextLine())
@@ -15,7 +21,7 @@ public class LineNumberer
       System.out.println(line);
       lineCount += 1;
     }
-    
+    scanner.close();
     System.out.println("Done");
   }
 }
