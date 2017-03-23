6a811f09b3f29e3d7e2dc75044e0a77a7f8000fd
Alex <ruski550@gmail.com>
2017-03-23 13:39:16 -0500

    got the second part of checkpoint 1 to work =)

--- project8/src/lab8/LineNumberer.java
+++ project8/src/lab8/LineNumberer.java
@@ -9,11 +9,28 @@ public class LineNumberer
 {
   public static void main(String[] args) throws FileNotFoundException
   {
-	 File  file = new File("C:\\Users\\ruski\\Documents\\Coms227\\coms227shit\\project7\\src\\lab7\\Deck.java");
-	 Scanner scanner = new Scanner(file);
+	 //File  file = new File("C:\\Users\\ruski\\Documents\\Coms227\\coms227shit\\project7\\src\\lab7\\Deck.java");
+	 //Scanner scanner = new Scanner(file);
     //Scanner scanner = new Scanner(System.in);
-    int lineCount = 1;
-
+   // int lineCount = 1;
+    
+    File f1 = new File("C:\\Users\\ruski\\Documents\\Coms227\\coms227shit\\project8\\story.txt");
+    Scanner s1 = new Scanner(f1);
+    int count = 0;
+    Scanner line = null;
+    
+    while(s1.hasNextLine()){
+    	line = new Scanner( s1.nextLine());
+    	while (line.hasNext()){
+    		line.next();
+    		count +=1;
+    	}
+    	System.out.println(count);
+    	count = 0;
+    	//s1.nextLine();
+    }
+    
+    /*
     while (scanner.hasNextLine())
     {
       String line = scanner.nextLine();
@@ -21,7 +38,8 @@ public class LineNumberer
       System.out.println(line);
       lineCount += 1;
     }
-    scanner.close();
+    */
+    s1.close();
     System.out.println("Done");
   }
 }
