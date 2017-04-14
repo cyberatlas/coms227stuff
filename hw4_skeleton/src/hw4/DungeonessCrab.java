f65439b2eb351fc4f2bd933fd76c0e8da6eef7be
Ruski <Ruski@DESKTOP-Q5CHNVV.richwoods.psd150.org>
2017-04-14 13:59:57 -0500

    .

---
+++ hw4_skeleton/src/hw4/DungeonessCrab.java
@@ -0,0 +1,7 @@
+package hw4;
+
+/**
+ * Created by Ruski on 4/14/2017.
+ */
+public class DungeonessCrab {
+}
---
+++ hw4_skeleton/src/hw4/Wall.java
@@ -0,0 +1,24 @@
+package hw4;
+
+import graph.Cell;
+
+import java.awt.*;
+
+/**
+ * Created by Ruski on 4/14/2017.
+ */
+public class Wall {
+    public Wall() {
+
+        /**
+         * TODO return the wall color as white
+         */
+        public static Color getColor(){
+            return Color;
+        }
+        public static void handle(Cell cell){
+        //TODO
+       }
+    
+    }
+}
